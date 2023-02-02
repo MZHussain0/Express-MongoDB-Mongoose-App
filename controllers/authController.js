57f85c96a474280fe75597f209b8da9d1a3297d9
Zakir Hussain <mzhussain092@gmail.com>
2023-02-02 11:33:31 +0530

    implemented user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,9 +25,10 @@ const handleLogin = async (req, res) => {
   // evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
+    const roles = Object.values(foundUser.roles);
     //create jwt
     const accessToken = jwt.sign(
-      { username: foundUser.username },
+      { UserInfo: { username: foundUser.username, roles: roles } },
       process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "30s" }
     );
@@ -51,7 +52,7 @@ const handleLogin = async (req, res) => {
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
       sameSite: "None",
-      secure: true,
+      secure: true, //remove this line ONLY for testing in thunder client
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.json({ accessToken });
